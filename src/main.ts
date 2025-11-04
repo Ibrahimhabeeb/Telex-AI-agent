76a111e7c504b172d8f0fceef63c47f7e9fe8697
Habeeb Ibrahim <hibrahim@softalliance.co.ng>
2025-11-04 10:21:23 +0100

    added

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- const port = process.env.PORT || 5000;
-await app.listen(port, '0.0.0.0');
- console.log(`🚀 Server running on port ${port}`);
-;
+  const port = Number(process.env.PORT) || 8080; // ensure it uses the Railway-assigned port
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server running on port ${port}`);
 }
+
 bootstrap();
